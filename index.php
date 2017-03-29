ba59524936e354d51efa88c1d9f474bc428668dd
Home PC <1606822214@qq.com>
2017-03-29 20:57:00 +0800

    修补SQL注入和Cookie httponly

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ if (isset ( $_COOKIE ['SESSIONID'] )) {
 		$o_date = new DateTime ( 'Asia/Chongqing' );
 		$n_nowTime = $o_date->format ( 'U' );
 		$S_Session_Id = md5 ( $_SERVER ['REMOTE_ADDR'] . $_SERVER ['HTTP_USER_AGENT'] . rand ( 0, 9999 ) . $n_nowTime );
-		setcookie ( 'VISITER', '', 0 );
-		setcookie ( 'SESSIONID', $S_Session_Id, 0 );
-		setcookie ( 'VALIDCODE', '2222', 0 );
+		setcookie ( 'VISITER', '', 0 ,'/','',false,true );
+		setcookie ( 'SESSIONID', $S_Session_Id, 0 ,'/','',false,true);
+		setcookie ( 'VALIDCODE', '222', 0 ,'/','',false,true);
 		echo ('<script>location=\'index.php\'</script>');
 		exit ( 0 );
 	}
@@ -16,9 +16,9 @@ if (isset ( $_COOKIE ['SESSIONID'] )) {
 	$o_date = new DateTime ( 'Asia/Chongqing' );
 	$n_nowTime = $o_date->format ( 'U' );
 	$S_Session_Id = md5 ( $_SERVER ['REMOTE_ADDR'] . $_SERVER ['HTTP_USER_AGENT'] . rand ( 0, 9999 ) . $n_nowTime );
-	setcookie ( 'VISITER', '', 0 );
-	setcookie ( 'SESSIONID', $S_Session_Id, 0 );
-	setcookie ( 'VALIDCODE', '222', 0 );
+	setcookie ( 'VISITER', '', 0 ,'/','',false,true );
+	setcookie ( 'SESSIONID', $S_Session_Id, 0 ,'/','',false,true);
+	setcookie ( 'VALIDCODE', '222', 0 ,'/','',false,true);
 	echo ('<script>location.reload()</script>');
 	exit ( 0 ); 
 }
